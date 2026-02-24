d5fb7dbc1572f339815bbc5a7b3cc64fa508ccc3
martijnfolmer <31698991+martijnfolmer@users.noreply.github.com>
2026-02-24 19:05:17 +0100

    grids
    - move cells to center screen
    - resize cells to a given size

--- scripts/Samples/GridSquare/cell_square.gd
+++ scripts/Samples/GridSquare/cell_square.gd
@@ -1,16 +1,21 @@
 extends Node2D
 class_name Cell
 
+@onready var sprite: Sprite2D = null
+
 enum CellType { EMPTY, ROAD, ROCK }
 
 var grid_x: int
 var grid_y: int
 var cell_type: CellType = CellType.EMPTY
 
-func init(x: int, y: int, t: CellType = CellType.EMPTY) -> void:
+func init(x: int, y: int, w: int, h: int, t: CellType = CellType.EMPTY) -> void:
 	grid_x = x
 	grid_y = y
 	set_type(t)
+	
+	# resize the square cell to be right size
+	resize(w, h)
 
 func set_type(t: CellType) -> void:
 	cell_type = t
@@ -18,3 +23,32 @@ func set_type(t: CellType) -> void:
 	# 	CellType.EMPTY: ...
 	# 	CellType.ROAD:  ...
 	# 	CellType.ROCK:  ...
+
+func resize(w: int, h: int) -> void:
+	if sprite == null:
+		sprite = _find_first_sprite2d(self)
+	
+	if sprite == null:
+		push_warning("resize(): No Sprite2D found.")
+		return
+
+	if sprite.texture == null:
+		push_warning("resize(): Sprite2D has no texture.")
+		return
+
+	var tex_size: Vector2 = sprite.texture.get_size()
+	if tex_size.x == 0.0 or tex_size.y == 0.0:
+		push_warning("resize(): Texture size is zero.")
+		return
+
+	sprite.scale = Vector2(float(w) / tex_size.x, float(h) / tex_size.y)
+
+func _find_first_sprite2d(root: Node) -> Sprite2D:
+	for c in root.get_children():
+		if c is Sprite2D:
+			return c
+		var found := _find_first_sprite2d(c)
+		if found != null:
+			return found
+	return null
+	
--- scripts/Samples/GridSquare/grid_spawn.gd
+++ scripts/Samples/GridSquare/grid_spawn.gd
@@ -15,12 +15,21 @@ func spawn_grid() -> void:
 	grid_state.clear()
 	grid_state.set_size(W, H)
 
+	var cam_center = get_camera_center_world_px()
+	var x1 = cam_center.x - cell_size.x * W / 2
+	var y1 = cam_center.y - cell_size.y * H / 2
+
 	for y in range(H):
 		for x in range(W):
 			var cell := cell_scene.instantiate() as Cell
 			add_child(cell)
 
-			cell.position = Vector2(x * cell_size.x, y * cell_size.y)
-			cell.init(x, y)
+			cell.position = Vector2(x1 + x * cell_size.x, y1 + y * cell_size.y)
+			cell.init(x, y, cell_size.x, cell_size.y)
 
 			grid_state.set_cell(x, y, cell)
+			
+# Find the center of the camera
+func get_camera_center_world_px() -> Vector2:
+	var rect = get_viewport().get_visible_rect()
+	return rect.position + rect.size/2

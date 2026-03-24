c2a96ae11d07b1d021b23a6fc51f0c33f3b7af9b
martijn.folmer <martijn.folmer@technolution.nl>
2026-03-24 08:50:11 +0100

    debris

--- scripts/Environment/debris.gd
+++ scripts/Environment/debris.gd
@@ -1,50 +1,102 @@
 extends Node2D
 
+@export_enum("wall", "glass") var cat = 0
 
-@export var sprite_1 : Texture2D
-@export var sprite_2 : Texture2D
-@export var sprite_3 : Texture2D
+@export_group("wall textures")
+@export var sprite_wall_1 : Texture2D
+@export var sprite_wall_2 : Texture2D
+@export var sprite_wall_3 : Texture2D
 
+@export_group("glass textures")
+@export var sprite_glass_1 : Texture2D
+@export var sprite_glass_2 : Texture2D
+@export var sprite_glass_3 : Texture2D
 
-# TODO: scale, movement, off off edge, creatoin of this and setting sprite_1,2,3
-# TODO: add to debris group
+@export_group("visual")
+## the initial scale of the debris
+@export var ini_scale : float = 1.0
+## The friction with which it stops moving
+@export var friction : float = 0.9
+
+'''
+todo:
+	- scale (done)
+	- glass and wall textures (done)
+	- add to debris group (done)
+	- initial rotation (done)
+	
+	- fall of the edge (non collision perhaps)
+	- give impulse (velocity added to current velocity, and a bit of rotation)
+	- update movement as well
+	- rotation update when we are moving
+
+	- initialization of the glass and wall
+	-- looking partially like it is part of the class
+	-- etending y scale as we are 'falling'
+	-- overall scale goes down, so it looks like we are falling on the ground
+'''
+
 
 var sprite
-
+var texture_1 : Texture2D
+var texture_2 : Texture2D
+var texture_3 : Texture2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	_set_debris_sprite()		# create the sprite for the debris from options
-	
-	pass # Replace with function body.
+	add_to_group("debris")
 
-func _set_debris_sprite() -> void:
+	_create_debris_sprite()		# create the sprite for the debris from options
+	_set_scale(ini_scale)	# the initial scale of the debris
+	rotation = randf() * 2 * PI	#random initial roation
+	
+
+func _create_debris_sprite() -> void:
 	# get the sprite2D
 	sprite = $Sprite2D
 
 	if sprite == null:
 		sprite = Sprite2D.new()
 		add_child(sprite)
+	
+	_set_debris_sprite()
 
+func _set_debris_sprite() -> void:
+	
+	if cat ==0:
+		texture_1 = sprite_wall_1
+		texture_2 = sprite_wall_2
+		texture_3 = sprite_wall_3
+	else:
+		texture_1 = sprite_glass_1
+		texture_2 = sprite_glass_2
+		texture_3 = sprite_glass_3
+	
+	
 	# edge case if we set no sprites
-	if sprite_1 == null and sprite_2 == null and sprite_3 == null:
+	if texture_1 == null and texture_2 == null and texture_3 == null:
 		return
 	
 	# choose a random sprite from the texture 2D	
-	sprite.texture = sprite_1
+	sprite.texture = texture_1
 	
 	var all_sprites = []
-	if sprite_1!= null:
-		all_sprites.append(sprite_1)
-	if sprite_2 != null:
-		all_sprites.append(sprite_2)
-	if sprite_3 != null:
-		all_sprites.append(sprite_3)
+	if texture_1!= null:
+		all_sprites.append(texture_1)
+	if texture_2 != null:
+		all_sprites.append(texture_2)
+	if texture_3 != null:
+		all_sprites.append(texture_3)
 	
 	var choice_index = randi() % all_sprites.size()
 	sprite.texture = all_sprites[choice_index]
-	
+
+## Set the scale of the sprite to the 	
+func _set_scale(_scale) -> void:
+	sprite.scale.x = _scale
+	sprite.scale.y = _scale
+
+
 	
 
 

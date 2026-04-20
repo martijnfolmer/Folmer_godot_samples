4fa303c2a882ead9af2eb6b64e7b8e9ea471bb0d
martijnfolmer <31698991+martijnfolmer@users.noreply.github.com>
2026-04-20 19:19:45 +0200

    different pitched single sfx

--- scripts/singletons/singleton_sfx.gd
+++ scripts/singletons/singleton_sfx.gd
@@ -13,12 +13,26 @@ const _STREAMS: Dictionary = {
 @onready var _player: AudioStreamPlayer = $AudioStreamPlayer
 
 
-func play_sfx(sfx_name: String) -> void:
+func play_sfx(sfx_name: String, from : float = 0.0, pitch : float = 1.0) -> void:
 	if not _STREAMS.has(sfx_name):
 		push_warning("Unknown sfx: %s" % sfx_name)
 		return
 	_player.stream = _STREAMS[sfx_name] as AudioStream
-	_player.play()
+	
+	_player.pitch_scale = pitch
+	_player.play(from)
+
+
+func play_sfx_rand_pitch(sfx_name: String, from : float = 0.0, random_pitch_min : float = 0.1, random_pitch_max : float = 2.0) -> void:
+	if not _STREAMS.has(sfx_name):
+		push_warning("Unknown sfx: %s" % sfx_name)
+		return
+	_player.stream = _STREAMS[sfx_name] as AudioStream
+	
+	var pitch = randf() * (random_pitch_max - random_pitch_min) + random_pitch_min
+	_player.pitch_scale = pitch
+	_player.play(from)
+
 
 
 #TODO: TESTING: make sfx happen
@@ -26,4 +40,5 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		var key_event := event as InputEventKey
 		if key_event.pressed and not key_event.echo and key_event.keycode == KEY_V:
-			play_sfx("hit1")
+			#play_sfx("hit1")
+			play_sfx_rand_pitch("hit1", 0)
